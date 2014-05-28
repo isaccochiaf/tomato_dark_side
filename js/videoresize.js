f057dfcfb67b86f9f91b8c5cfbb5ebcdac8cb537
isacco chiaf <isacco.chiaf@gmail.com>
2014-05-28 15:46:11 +0200

    faffafa

--- js/videoresize.js
+++ js/videoresize.js
@@ -78,3 +78,23 @@ videojs('my_video_4').ready(function(){
       // Then on resize call resizeVideoJS()
        window.addEventListener("resize", resizeVideoJS, false);
     });
+
+videojs('my_video_5').ready(function(){
+      console.log(this.options()); //log all of the default videojs options
+      
+       // Store the video object
+      var myPlayer = this, id = myPlayer.id();
+      // Make up an aspect ratio
+      var aspectRatio = 9/16; 
+
+      function resizeVideoJS(){
+        var width = document.getElementById(id).parentElement.offsetWidth;
+        myPlayer.width(width).height( width * aspectRatio );
+
+      }
+      
+      // Initialize resizeVideoJS()
+      resizeVideoJS();
+      // Then on resize call resizeVideoJS()
+       window.addEventListener("resize", resizeVideoJS, false);
+    });
